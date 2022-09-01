da3724f48c195c3956522a8a5eec3936e944c196
RafaelFreitas <raf.mec.ba@gmail.com>
2022-09-01 09:42:49 -0300

    S5-06

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-let caixaDeSuprimentos = []
-let higiente = ["sabão", "escova", "papel", "pasta", "pente"]
-let alimentacao = ["carne", "fruta", "pão", "suco", "biscoito"]
-let cuidadosMedicos = ["mercurio", "mertiolate", "band-aid", "gase", "esparadrapo"]
-let lazer = ["bola", "taco", "espada", "escudo", "playboy"]
-let importante = ["isqueiro", "lanterna", "barraca", "lampião", "querosene"]
-// let besteira = ["corrente", "spike"]
+let garden = [
+    [0,0,0,'G'],
+    [0,0,'G',0],
+    [0,'G',0,0],
+    ['G',0,0,0]
+    ]
 
-caixaDeSuprimentos.push(higiente)
-caixaDeSuprimentos.push(alimentacao)
-caixaDeSuprimentos.push(cuidadosMedicos)
-caixaDeSuprimentos.push(lazer)
-// caixaDeSuprimentos.push(importante)
-// caixaDeSuprimentos.push(besteira)
-
-
-console.table(caixaDeSuprimentos)
-
-function acampar(lista) {
-    if (lista.length == 5) {
-        for (let i = 0; i < lista.length; i++) {
-            console.log(lista[i])
-            if (lista[i].length == 5) {
-                return "Podemos ir acampar."
-            } else if (lista[i].length > 5) {
-                return "Tem itens demais, não precisamos de tantos."
-            } else if (lista[i].length < 5) {
-                return "Precisamos de mais itens desta seção."
+function cortador(lista){
+    for(let i=0;i<lista.length;i++){
+        for(let j=0;j<lista[i].length;j++){
+            if(lista[i][j]=="G"){
+                lista[i][j]=0
             }
-
         }
-        return lista[i]
-    } else if (lista.length < 5) {
-        return "Algum amigo ainda não retornou com os itens."
-    } else if (lista.length > 5) {
-        return "Acho que temos um intruso."
     }
+    return lista
 }
 
-
-
-console.log(acampar(caixaDeSuprimentos))
-
+console.table(cortador(garden))
